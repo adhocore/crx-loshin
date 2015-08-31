526a4b8daa474e6860421cfd66c82eb3998edd6d
Jitendra Adhikari <jiten.adhikary@gmail.com>
2015-08-31 15:14:17 +0545

    Reload self on settings change to force new settings

--- background.js
+++ background.js
@@ -1,87 +1,92 @@
 // globals for easy access by interval'ed callback
-var LastIcon = 'default', notifer, 
-    icon = 'default', within = '',
-    group = 'group_1';
+var LastIcon = 'default',
+  notifer,
+  icon = 'default',
+  within = '',
+  group = 'group_1';
 
 function swapIcon() {
-    var schedule = JSON.parse(localStorage.loshinCache || '[0]')[0], newDate = new Date(),
-        today = 'sunday monday tuesday wednesday thursday friday saturday'.split(' ')[newDate.getDay()],
-        alertThreshold = localStorage.loshinAlertBefore * 1 || 15, 
-        minutes = [], blackOut, minute = newDate.getHours() * 60 + newDate.getMinutes(),
-        group = localStorage.loshinMyGroup || 'group_1'
-    ;
+  var schedule = JSON.parse(localStorage.loshinCache || '[0]')[0],
+    newDate = new Date(),
+    today = 'sunday monday tuesday wednesday thursday friday saturday'.split(' ')[newDate.getDay()],
+    alertThreshold = localStorage.loshinAlertBefore * 1 || 15,
+    minutes = [],
+    blackOut, minute = newDate.getHours() * 60 + newDate.getMinutes(),
+    group = localStorage.loshinMyGroup || 'group_1';
 
-    if (! schedule) {
-        chrome.browserAction.setIcon({ path: 'icons/default.png' });
-        chrome.browserAction.setTitle({ title:'loshin: Could not fetch data, Click to reload' });
-        return;
-    }
+  if (!schedule) {
+    chrome.browserAction.setIcon({
+      path: 'icons/default.png'
+    });
+    chrome.browserAction.setTitle({
+      title: 'loshin: Could not fetch data, Click to reload'
+    });
+    return;
+  }
 
-    blackOut = schedule[group][today];
-    ['morning', 'evening'].forEach(function(shift){
-        blackOut[shift].split('-').forEach(function(tym){
-            var tyms = tym.split(':');
-            minutes.push((tyms[0] < 1 ? 24 : tyms[0]) * 60 + tyms[1] * 1);              
-        });
-    }); 
+  blackOut = schedule[group][today];
+  ['morning', 'evening'].forEach(function(shift) {
+    blackOut[shift].split('-').forEach(function(tym) {
+      var tyms = tym.split(':');
+      minutes.push((tyms[0] < 1 ? 24 : tyms[0]) * 60 + tyms[1] * 1);
+    });
+  });
 
-    // quick fix:
-    if (minutes[0] > minutes[1]) minutes[1] += 1440;
-    if (minutes[2] > minutes[3]) minutes[3] += 1440;
-    
-    if ( ( (diff1 = minutes[0] - minute) >= 0 && diff1 < alertThreshold) 
-        ||
-        ( (diff1 = minutes[2] - minute) >= 0 && diff1 < alertThreshold)
-    ) icon = 'going';
-    else if ( ( (diff2 = minutes[1] - minute) >= 0 && diff2 < alertThreshold) 
-        ||
-        ( (diff2 = minutes[3] - minute) >= 0 && diff2 < alertThreshold)
-    ) icon = 'coming';
-    else if (minute < minutes[0] || minute > minutes[3] || (minute > minutes[1] && minute < minutes[2]))
-        icon = 'on';
-    else icon = 'off';
-        
-    if (icon == 'going') within = ' within ' + diff1 + ' minute(s)';
-    else if (icon == 'coming') within = ' within ' + diff2 + ' minute(s)';
-    else within = '';
+  // quick fix:
+  if (minutes[0] > minutes[1]) minutes[1] += 1440;
+  if (minutes[2] > minutes[3]) minutes[3] += 1440;
 
-    chrome.browserAction.setIcon({ path: 'icons/' + icon + '.png' });
-    chrome.browserAction.setTitle({ title:'loshin: Light is ' + icon + within });
+  if (((diff1 = minutes[0] - minute) >= 0 && diff1 < alertThreshold) ||
+    ((diff1 = minutes[2] - minute) >= 0 && diff1 < alertThreshold)
+  ) icon = 'going';
+  else if (((diff2 = minutes[1] - minute) >= 0 && diff2 < alertThreshold) ||
+    ((diff2 = minutes[3] - minute) >= 0 && diff2 < alertThreshold)
+  ) icon = 'coming';
+  else if (minute < minutes[0] || minute > minutes[3] || (minute > minutes[1] && minute < minutes[2]))
+    icon = 'on';
+  else icon = 'off';
 
-    if (icon !== LastIcon) {
-        clearInterval(notifer);
-        notifer = null;
+  if (icon == 'going') within = ' within ' + diff1 + ' minute(s)';
+  else if (icon == 'coming') within = ' within ' + diff2 + ' minute(s)';
+  else within = '';
+
+  chrome.browserAction.setIcon({
+    path: 'icons/' + icon + '.png'
+  });
+  chrome.browserAction.setTitle({
+    title: 'loshin: Light is ' + icon + within
+  });
+
+  if (icon !== LastIcon) {
+    clearInterval(notifer);
+    notifer = null;
+    notify();
+  }
+
+  function notify() {
+    LastIcon = icon;
+    // revoke as per https://developer.mozilla.org/en/docs/Web/API/notification
+    var notif = new Notification(
+      new Date().toLocaleTimeString().replace(/:\d+ /, ' '), {
+        icon: 'icons/' + icon + '.png',
+        body: 'loshin: Light is ' + icon + within
+      }
+    );
+  }
+
+  if (!notifer) {
+    var inter = localStorage.loshinNoticeInterval;
+    notifer = setInterval(
+      function() {
         notify();
-    }
-
-    function notify() {
-        LastIcon = icon;
-        // revoke as per https://developer.mozilla.org/en/docs/Web/API/notification
-        var notif = new Notification(
-                new Date().toLocaleTimeString().replace(/:\d+ /, ' '),
-                {
-                    icon: 'icons/' + icon + '.png',
-                    body: 'loshin: Light is ' + icon + within
-                }
-            )
-        ;
-    }
-
-    if (! notifer) {
-        var inter = localStorage.loshinNoticeInterval;
-        notifer = setInterval(
-            function() {
-                notify();
-            }, 
-            (inter || 30) * 60000
-        );
-    }
+      }, (inter || 30) * 60000
+    );
+  }
 }
 
-function ping() {   
-    var tymr = setInterval(swapIcon, 10000);
-    swapIcon();     
+function ping() {
+  var tymr = setInterval(swapIcon, 10000);
+  swapIcon();
 }
 
 ping();
-
--- loshin.js
+++ loshin.js
@@ -1,116 +1,104 @@
 var Loshin = Loshin || {
-    
-    init: function(force){
-        if (force || (localStorage.loshinCacheDate != new Date().toLocaleDateString())) {
-            var req = new XMLHttpRequest();
-            req.open("GET", 'http://api.battigayo.com/', true);
-            req.onload = this.load.bind(this);
-            req.onerror = this.cache.bind(this);
-            req.send(null);
-        } else this.cache();
-    },
-    
-    load: function(xhrEvt){
-        var data = xhrEvt.target.responseText, 
-            schedule = JSON.parse(data || '[0]')
-        ;
-        localStorage.loshinCache = data;
-        localStorage.loshinCacheDate = new Date().toLocaleDateString()
-        this.build(schedule[0]);
-    },
-    
-    cache: function(){
-        var schedule = JSON.parse(localStorage.loshinCache || '[0]');
-        this.build(schedule[0]);
-    },
-    
-    build: function(schedule) {
-        var myGroup = localStorage.loshinMyGroup || 'group_1',
-            days = 'sunday monday tuesday wednesday thursday friday saturday'.split(' ')
-            today = new Date().getDay(), tHead = ''
-        ;
 
-        days.forEach(function(day, j){
-            tHead += '<th' + (j == today ? ' class="today" >' : '>') 
-                + day.charAt(0).toUpperCase() + day.slice(1) + '</th>'
-            ;
-        });
-        
-        $('my-group-table').innerHTML 
-            = $('other-group-table').innerHTML 
-            = '<tr><th>Group</th>' + tHead + '</tr>'
-        ;
+  init: function(force) {
+    if (force || (localStorage.loshinCacheDate != new Date().toLocaleDateString())) {
+      var req = new XMLHttpRequest();
+      req.open("GET", 'http://api.battigayo.com/', true);
+      req.onload = this.load.bind(this);
+      req.onerror = this.cache.bind(this);
+      req.send(null);
+    } else this.cache();
+  },
 
-        $('my-group').value = myGroup;
-        $('alert-before').value = localStorage.loshinAlertBefore || 15;
-        $('notice-interval').value = localStorage.loshinNoticeInterval || 30;
-        
-        if (! schedule) {
-            errMsg = '<td colspan="8">Error loading, Please check connection </td>';
-            $('my-group-table').innerHTML += errMsg;
-            $('other-group-table').innerHTML += errMsg;
-            return;
-        }
+  load: function(xhrEvt) {
+    var data = xhrEvt.target.responseText,
+      schedule = JSON.parse(data || '[0]');
+    localStorage.loshinCache = data;
+    localStorage.loshinCacheDate = new Date().toLocaleDateString()
+    this.build(schedule[0]);
+  },
 
-        $('updated').innerHTML = schedule.updated.nepali + ' BS';
-        $('updated').title = schedule.updated.english + ' AD';
+  cache: function() {
+    var schedule = JSON.parse(localStorage.loshinCache || '[0]');
+    this.build(schedule[0]);
+  },
 
-        for (i = 1; i <= 7; i++) {
-            var group = 'group_' + i, 
-                outHtml = '<tr><td>' + i + '</td>'
-            ;
-            for (j = 0; j < 7; j++) {
-                outHtml += '<td' + (j == today ? ' class="today" >' : '>') 
-                    + schedule[group][days[j]]['morning'] 
-                    + '<br />' + schedule[group][days[j]]['evening'] 
-                    + '</td>';
-            }
-            outHtml += '</tr>';
-            $(group == myGroup ? 'my-group-table' : 'other-group-table').innerHTML += outHtml;
-        }
+  build: function(schedule) {
+    var myGroup = localStorage.loshinMyGroup || 'group_1',
+      days = 'sunday monday tuesday wednesday thursday friday saturday'.split(' ')
+    today = new Date().getDay(), tHead = '';
 
+    days.forEach(function(day, j) {
+      tHead += '<th' + (j == today ? ' class="today" >' : '>') + day.charAt(0).toUpperCase() + day.slice(1) + '</th>';
+    });
+
+    $('my-group-table').innerHTML = $('other-group-table').innerHTML = '<tr><th>Group</th>' + tHead + '</tr>';
+
+    $('my-group').value = myGroup;
+    $('alert-before').value = localStorage.loshinAlertBefore || 15;
+    $('notice-interval').value = localStorage.loshinNoticeInterval || 30;
+
+    if (!schedule) {
+      errMsg = '<td colspan="8">Error loading, Please check connection </td>';
+      $('my-group-table').innerHTML += errMsg;
+      $('other-group-table').innerHTML += errMsg;
+      return;
     }
+
+    $('updated').innerHTML = schedule.updated.nepali + ' BS';
+    $('updated').title = schedule.updated.english + ' AD';
+
+    for (i = 1; i <= 7; i++) {
+      var group = 'group_' + i,
+        outHtml = '<tr><td>' + i + '</td>';
+      for (j = 0; j < 7; j++) {
+        outHtml += '<td' + (j == today ? ' class="today" >' : '>') + schedule[group][days[j]]['morning'] + '<br />' + schedule[group][days[j]]['evening'] + '</td>';
+      }
+      outHtml += '</tr>';
+      $(group == myGroup ? 'my-group-table' : 'other-group-table').innerHTML += outHtml;
+    }
+
+  }
 };
 
 function $(i) {
-    return document.getElementById(i);
+  return document.getElementById(i);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   Loshin.init();
 });
 
-$('save-setting').addEventListener('click', function(){
-    var alertBefore = $('alert-before').value * 1,
-        noticeInterval = $('notice-interval').value * 1
-    ;
-    localStorage.loshinMyGroup = $('my-group').value;
-    localStorage.loshinAlertBefore =  alertBefore < 10 
-        ? 10 : alertBefore > 90 ? 90 : alertBefore;
-    localStorage.loshinNoticeInterval =  noticeInterval < 30 
-        ? 30 : noticeInterval > 180 ? 180 : noticeInterval;
-    
-    $('save-setting').value = 'Saved';
-    
-    if ($('force-update').checked) {
-        Loshin.init(true);
-        document.location.hash = 'box-three';
-        $('force-update').checked = false;
-    } 
-        
-    var saver = setTimeout(function(){
-        $('save-setting').value = 'Save';
-        clearInterval(saver);
-    }, 1234);
-    
+$('save-setting').addEventListener('click', function() {
+  var alertBefore = $('alert-before').value * 1,
+    noticeInterval = $('notice-interval').value * 1;
+  localStorage.loshinMyGroup = $('my-group').value;
+  localStorage.loshinAlertBefore = alertBefore < 10 ? 10 : alertBefore > 90 ? 90 : alertBefore;
+  localStorage.loshinNoticeInterval = noticeInterval < 30 ? 30 : noticeInterval > 180 ? 180 : noticeInterval;
+
+  $('save-setting').value = 'Saved';
+
+  if ($('force-update').checked) {
+    Loshin.init(true);
+    document.location.hash = 'box-three';
+    $('force-update').checked = false;
+  }
+
+  var saver = setTimeout(function() {
+    $('save-setting').value = 'Save';
+    clearInterval(saver);
+  }, 1234);
+
+  // reload itself
+  chrome.runtime.reload();
 })
 
 var tabAnchors = document.getElementsByClassName('tab-anchor');
 for (i = 0; i < tabAnchors.length; i++) {
-    tabAnchors[i].addEventListener('click', function(){
-        for (j = 0; j < tabAnchors.length; j++) {
-            tabAnchors[j].className = tabAnchors[j].className.replace(' active', '');
-        }
-        this.className += ' active';
-    });
+  tabAnchors[i].addEventListener('click', function() {
+    for (j = 0; j < tabAnchors.length; j++) {
+      tabAnchors[j].className = tabAnchors[j].className.replace(' active', '');
+    }
+    this.className += ' active';
+  });
 }
